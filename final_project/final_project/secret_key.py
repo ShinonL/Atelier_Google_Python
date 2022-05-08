95cd3ecb631c8a8a013d1df8a76298e5d4f80792
Laura Nadu <laura@x-land.ro>
2022-05-08 13:40:12 +0300

    Renamed Project

---
+++ final_project/final_project/secret_key.py
@@ -0,0 +1,1 @@
+SECRET_KEY_VALUE = 'django-insecure-qjj%1qrenn#_%uz4uww7jv4gva6#s8w-3^sori)@mosgq)+9_a'
--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -1,4 +1,4 @@
-"""first_project URL Configuration
+"""final_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
